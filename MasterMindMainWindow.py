5a70dcf5dd589966ca6a53054b936238cac4b2d5
CreativePhilip <2hepy9XB4Fqx>
2019-09-01 20:03:52 +0200

    dynamiczne dodawanie przycisków + handlerów

--- MasterMindMainWindow.py
+++ MasterMindMainWindow.py
@@ -10,6 +10,8 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi(r"master_mind_main_gui.ui", self)
+        self.dialog_input = None
+
         self.actual_game = None
         self.dialog = NewGameDialog(self)
 
@@ -18,16 +20,37 @@ class MainWindow(QMainWindow):
 
         self.player_input_send_button.clicked.connect(self.player_input_send_button_handler)
 
+        # self.gridLayout = QGridLayout()
+        # self.scrollArea.setLayout(self.gridLayout)
+
+    def buttonHandlerFactory(self, param):
+        def handle():
+            input_text = self.player_input.text()
+            input_text += f"{param},"
+            self.player_input.setText(input_text)
+        return handle
+
+    def generateButtons(self, count):
+        row = 0
+        for i in range(count):
+            if i % 3 == 0:
+                row += 1
+            button = QPushButton(str(i + 1))
+            self.gridLayout.addWidget(button, row, i % 3)
+            button.clicked.connect(self.buttonHandlerFactory(i + 1))
+
     def player_input_send_button_handler(self):
         player_input = self.player_input.text()
         result = self.actual_game.single_player_input(player_input)
+
         new_variable = f"input: {player_input} result: {result}"
+
         self.game_history_listwidget.addItem(new_variable)
         self.player_input.setText("")
 
-
     def new_game_action_handler(self):
         self.dialog.show()
+
 
     @staticmethod
     def quit_action_handler():
@@ -39,7 +62,6 @@ class NewGameDialog(QDialog):
         super(NewGameDialog, self).__init__()
         loadUi(r"master_mind_new_game.ui", self)
         self.main_window = main_window
-
         self.start_game_button.clicked.connect(self.start_game_handler)
 
     def start_game_handler(self):
@@ -50,12 +72,7 @@ class NewGameDialog(QDialog):
         except ValueError:
             pass
 
-        elements = ""
-        for i in range(1, b+1):
-            elements += f"[{i}] "
-
-        print(elements)
-        self.main_window.symbols_quantity_label.setText(elements)
+        self.main_window.generateButtons(b)
         self.main_window.actual_game = GameProcess.Game(a, b, c)
         self.hide()
 
